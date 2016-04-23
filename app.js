c724554b433950e70c1e20b6c0e382c3eb123bfe
soivi <soivi.lauri@gmail.com>
2016-04-23 14:52:46 +0300

    First commit

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var tasks = require('./routes/tasks');
+var mongoose = require('mongoose');
 
 var app = express();
+
+// Mongo
+mongoose.connect('mongodb://localhost/todoapp', function(err) {
+    if(err) {
+        console.log('connection error', err);
+    } else {
+        console.log('connection successful');
+    }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +34,7 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/tasks', tasks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
